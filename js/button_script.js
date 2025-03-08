c055a78e43d2ca60b824adeba1e27e4e0b5403b1
joelrael-dev <joel.rael.999@gmail.com>
2025-03-08 16:22:09 -0600

    dev

--- js/button_script.js
+++ js/button_script.js
@@ -1,41 +1,8 @@
-class CS_GalleryFilter {
-	filtersSelector = '.cs-button';
-	galleriesSelector = '.cs-gallery';
-	activeClass = 'cs-active';
-	hiddenClass = 'cs-hidden';
+// Add a class to the body when the toggle switch is clicked
 
-	constructor() {
-		this.$galleries = document.querySelectorAll(this.galleriesSelector);
-		const $filters = document.querySelectorAll(this.filtersSelector);
+document.getElementById('toggle').addEventListener('change', function () {
 
-		this.onClick($filters[0]);
+  document.body.classList.toggle('dark-mode');
 
-		for (const $filter of $filters) {
-			$filter.addEventListener('click', () => this.onClick($filter));
-		}
-	}
+});
 
-	onClick($filter) {
-		this.filter($filter.dataset.filter);
-
-		const { activeClass } = this;
-
-		this.$activeFilter?.classList.remove(activeClass);
-		$filter.classList.add(activeClass);
-
-		this.$activeFilter = $filter;
-	}
-
-	filter(filter) {
-		const showAll = filter == 'all';
-		const { hiddenClass } = this;
-
-		for (const $gallery of this.$galleries) {
-			const show = showAll || $gallery.dataset.category == filter;
-			$gallery.classList.toggle(hiddenClass, !show);
-		}
-	}
-}
-
-new CS_GalleryFilter();
-                                
